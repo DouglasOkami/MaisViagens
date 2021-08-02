84cc8e49844f0a08adc71204a9a82e33bf3ab569
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-08-02 09:47:15 -0300

    corrigindo o layote

--- + Viagens/Home/Controller/ViewController.swift
+++ + Viagens/Home/Controller/ViewController.swift
@@ -41,6 +41,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.labelPreco.text = viagemAtual.preco
         cell.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
         
+        cell.imagemViagem.layer.cornerRadius = 10
+        cell.imagemViagem.layer.masksToBounds = true
+        
         return cell
     }
  
