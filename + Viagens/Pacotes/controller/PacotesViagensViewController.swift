bac136bca77949f7d9bb9eae4594864f6a42c028
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-08-03 08:05:11 -0300

    trabalhando com UIstackview 1/2

---
+++ + Viagens/Pacotes/controller/PacotesViagensViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PacotesViagensViewController.swift
+//  + Viagens
+//
+//  Created by r.da.silva.ferreira on 02/08/21.
+//
+
+import UIKit
+
+class PacotesViagensViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
