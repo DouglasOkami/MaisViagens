7bd30a9da740fe1e52b37ea603154c3fe9920c45
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-08-05 10:16:47 -0300

    criado várias StackVIew

---
+++ + Viagens/Model/PacoteViagem.swift
@@ -0,0 +1,15 @@
+//
+//  PacoteViagem.swift
+//  + Viagens
+//
+//  Created by r.da.silva.ferreira on 05/08/21.
+//
+
+import UIKit
+
+class PacoteViagem: NSObject {
+    let nomeDoHotel: String
+    let descricao: String
+    let dataViagem: String
+    let viagem: Viagem
+}
